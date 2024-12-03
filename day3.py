fd8e71183f655a8ddefaca6010ca49b2c7b8f876
hasobi <13960580+hasobi@users.noreply.github.com>
2024-12-03 08:10:22 -0500

    day 3 answer

--- day3.py
+++ day3.py
@@ -1,34 +1,73 @@
+from pathlib import Path
 import re
 
 
-def main():
-    with open("day3.txt", "rt", encoding="utf-8") as f:
-        memory = f.read()
-
-    pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
-    sum_of_muls = sum(
-        int(match.group(1)) * int(match.group(2)) for match in pattern.finditer(memory)
-    )
-
-    print(f"Part 1: {sum_of_muls}")
-
-    sum_of_muls = 0
-    do_start, do_end = 0, memory.find("don't()")
-
-    while True:
-        sum_of_muls += sum(
-            int(match.group(1)) * int(match.group(2))
-            for match in pattern.finditer(memory, do_start, do_end if do_end != -1 else len(memory))
-        )
-
-        if do_end == -1:
-            break
-
-        do_start = memory.find("do()", do_end)
-        do_end = memory.find("don't()", do_start)
-
-    print(f"Part 2: {sum_of_muls}")
+def get_data(filepath: Path) -> str:
+    with open(filepath, "r") as f:
+        data = f.read()
+    return data
 
 
-if __name__ == "__main__":
-    main()
+def multiply(ops: str) -> int:
+    num_pattern = r"mul\((\d*),(\d*)\)"
+    results = re.match(num_pattern, ops)
+    return int(results.group(1)) * int(results.group(2))
+
+
+def parse_and_process_mul(instructions: str) -> int:
+    mul_pattern = r"mul\(\d*,\d*\)"
+    total = 0
+    for val in re.findall(mul_pattern, instructions):
+        total += multiply(val)
+    return total
+
+
+def parse_and_process_with_dos_and_donts(instructions: str) -> int:
+    do_check_pattern = r"(mul\(\d*,\d*\))|(do\(\))|(don't\(\))"
+    total = 0
+    dont_flag = False
+    for val in re.findall(do_check_pattern, instructions):
+        match val:
+            case ("", "do()", ""):
+                if dont_flag:
+                    dont_flag = False
+            case ("", "", "don't()"):
+                dont_flag = True
+            case (ops, "", ""):
+                if dont_flag:
+                    continue
+                total += multiply(ops)
+    return total
+
+
+def part_a_example():
+    example1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    result = parse_and_process_mul(example1)
+    print(result)  # 161
+
+
+def part_a():
+    fp = Path(r"day3.txt")
+    instructions = get_data(fp)
+    result = parse_and_process_mul(instructions)
+    print(result)
+
+
+def part_b_example():
+    example2 = r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    result = parse_and_process_with_dos_and_donts(example2)
+    print(result)  # 48
+
+
+def part_b():
+    fp = Path(r"day3.txt")
+    instructions = get_data(fp)
+    result = parse_and_process_with_dos_and_donts(instructions)
+    print(result)
+
+
+if __name__ == '__main__':
+    part_a_example()
+    part_a()
+    part_b_example()
+    part_b()
